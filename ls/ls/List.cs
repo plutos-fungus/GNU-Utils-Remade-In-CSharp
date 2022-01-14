ebc4263ccf51eb19df5f59e2a09f2d63f1d51f3d
Plutos-Fungus <rasmus1070@gmail.com>
2022-01-14 17:45:13 +0100

    Added ls

---
+++ ls/ls/List.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ls
+{
+    public class Lister
+    {
+        public void FileList(string x)
+        {
+            DirectoryInfo info = new DirectoryInfo(x);
+            
+            string[] files = Directory.GetFileSystemEntries(x);
+            foreach (string s in files)
+            {
+                Console.WriteLine(s);
+            }
+            
+        }
+        public void DirectoryList(string x)
+        {
+            string[] dirs = Directory.GetDirectories(x, "*", SearchOption.TopDirectoryOnly);
+            foreach (string s in dirs)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        public void FlagParser(string x)
+        {
+            Console.WriteLine("hej");
+        }
+    }
+}
---
+++ ls/ls/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ls
+{
+    class ls
+    {
+        static void Main(string[] args)
+        {
+            int fileFlag = 0;
+            string fileName;
+            Lister lister = new Lister();
+            foreach (string str in args)
+            {
+                if (str.StartsWith('-'))
+                {
+                    // Do things;
+                    //flagParser(str)???
+                    //Console.WriteLine("hej");
+                    foreach(string str2 in str.Split('-'))
+                    {
+                        fileFlag++;
+                        Console.WriteLine(fileFlag);
+                    }
+                }
+                else
+                {
+                    fileName = str;
+                    Console.WriteLine(str);
+                    //fileFlag++;
+                    Console.WriteLine("=============================================");
+                    lister.FileList(str);
+                    Console.WriteLine("=============================================");
+                    lister.DirectoryList(str);
+                    
+                }
+                //if (fileFlag > 1)
+                //{
+                //    Console.WriteLine("Only one directory at a time");
+                //}
+            }
+        }
+    }
+}
