65d998ad98e05c4a47187468503bb7b3d0aa7cf3
Plutos-Fungus <rasmus1070@gmail.com>
2022-01-24 16:31:28 +0100

    Fixed the IndexOutOfRange problem

--- cp/cp/cp/Copy.cs
+++ cp/cp/cp/Copy.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace cp
 {
-    internal class Copy
+    public class Copy
     {
+        string sourcePath = "";
+        string targetPath = "";
+        public void FlagParser(List<string> x, List<string> y)
+        {
+            if (x.Count > 1)
+            {
+                if (x[0] == "-")
+                {
+                    Console.WriteLine("Something something file flag");
+                }
+            }
+            else
+            {
+                sourcePath = Convert.ToString(y[0]);
+                targetPath = Convert.ToString(y[1]);
+                Console.WriteLine(sourcePath);
+                Console.WriteLine(targetPath);
+            }
+        }
     }
 }
--- cp/cp/cp/Program.cs
+++ cp/cp/cp/Program.cs
@@ -4,10 +4,22 @@
     {
         static void Main(string[] args)
         {
-            string sourcePath;
-            string targetPath;
-
-
+            Copy copy = new Copy();
+            List<string> pathList = new List<string>();
+            List<string> flagList = new List<string>();
+            foreach (string str in args)
+            {
+                if (str.StartsWith('-'))
+                {
+                    flagList.Add(str);
+                }
+                else if (!str.StartsWith('-'))
+                {
+                    Console.WriteLine(str);
+                    pathList.Add(str);
+                }
+            }
+            copy.FlagParser(flagList, pathList);
         }
     }
 }
